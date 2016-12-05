e02ddbcca671225eef9a7951c810cd6dc40e75fc
nemosx <root.michael.r@gmail.com>
2016-12-04 20:46:17 -0500

    refactor part 1 solution

--- src/Day2/part1.js
+++ src/Day2/part1.js
@@ -2,44 +2,54 @@
  * Created by Michael Root on 12/4/2016.
  */
 const fs = require('fs');
-
-const startingButton = [1, 1];
+var part1 = true;
+var startingButton = [2, 2];
 
 const keyPad =
-    [   [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+    [   [-1, -1, -1, -1, -1],
+        [-1, 1, 2, 3, -1],
+        [-1, 4, 5, 6, -1],
+        [-1, 7, 8, 9, -1],
+        [-1, -1, -1, -1, -1]
     ];
+
+const altKeyPad = [
+    [-1, -1, -1,   1, -1, -1, -1, -1],
+    [-1, -1,  2,   3,  4, -1, -1],
+    [-1,  5,  6,   7,  8,  9, -1],
+    [-1, -1, 'A', 'B', 'C', -1, -1],
+    [-1, -1, -1,  'D', -1,  -1,  -1],
+    [-1, -1, -1,  -1, 1, -1, -1, -1]
+];
 
 const buttonOperations = {
     'U': function (previousButton) {
-        if (previousButton[1] !== 0) {
-            previousButton[1]--;
-        }
-        return previousButton;
+        var nextButton = [previousButton[0], previousButton[1] - 1];
+        return inBounds(nextButton) ? nextButton : previousButton.slice();
     },
     'D': function (previousButton) {
-        if (previousButton[1] !== 2) {
-            previousButton[1]++;
-        }
-        return previousButton;
+        var nextButton = [previousButton[0], previousButton[1] + 1];
+        return inBounds(nextButton) ? nextButton : previousButton.slice();
     },
     'L': function (previousButton) {
-        if (previousButton[0] !== 0) {
-            previousButton[0]--;
-        }
-        return previousButton;
+        var nextButton = [previousButton[0] - 1, previousButton[1]];
+        return inBounds(nextButton) ? nextButton : previousButton.slice();
     },
     'R': function (previousButton) {
-        if (previousButton[0] !== 2) {
-            previousButton[0]++;
-        }
-        return previousButton;
+        var nextButton = [previousButton[0] + 1, previousButton[1]];
+        return inBounds(nextButton) ? nextButton : previousButton.slice();
     }
 };
 
+function inBounds(button) {
+
+    if (part1) {
+        return keyPad[button[1]][button[0]] !== -1;
+    }
+    return altKeyPad[button[1]][button[0]] !== -1;
+}
+
 function processInstruction(previousButton, instruction) {
-    console.log('instruction', instruction);
     return buttonOperations[instruction](previousButton);
 }
 
@@ -49,6 +59,8 @@ fs.readFile('input-part-1.txt', 'utf-8', (err, data) => {
 
     function processLine(presses, line) {
         var result = line.split('').reduce(processInstruction, startingButton);
+
+        startingButton = result;
 
         presses.push([result[0], result[1]]);
 
