b593458633a336581ff4487e0a466fd28ea9c8c6
nemosx <root.michael.r@gmail.com>
2016-12-05 19:58:57 -0500

    Day 3 Part 2 Solution

--- src/Day3/part1.js
+++ src/Day3/part1.js
@@ -1,17 +1,39 @@
 const fs = require('fs');
+var part1 = false;
+
+function isValidTriangle(triangleValues) {
+    const maxValue = Math.max(...triangleValues);
+    const sum = triangleValues.reduce((sum, val) => {return sum + val;}, 0);
+
+    return maxValue < sum - maxValue;
+}
+
+function countValidTriangles(triangles) {
+    return triangles.reduce((validCount, triangle) => {
+        return isValidTriangle(triangle) ? validCount + 1 : validCount;
+    }, 0);
+}
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    const numValid = data.split('\n').reduce((validCount, triangle) => {
-        let triangleValues = triangle.trim().split(' ').map(val => {return Number(val);});
+    var triangleArrays = data.split('\n').map(line => {
 
-        const maxValue = Math.max(...triangleValues);
+        return line.split(' ').map(val => {
+            return Number(val);
+        });
+    });
 
-        const sum = triangleValues.reduce((sum, val) => {return sum + val;}, 0);
+    if (part1) {
+        console.log('Part 1 Answer:', countValidTriangles(triangleArrays));
+    } else {
+        const totalRows = triangleArrays.length;
+        var altTriangles = [];
 
-        const isValidTriangle = maxValue < sum - maxValue;
+        for (let i = 0; i < totalRows; i += 3) {
+            altTriangles.push([triangleArrays[i][0], triangleArrays[i+1][0], triangleArrays[i+2][0]]);
+            altTriangles.push([triangleArrays[i][1], triangleArrays[i+1][1], triangleArrays[i+2][1]]);
+            altTriangles.push([triangleArrays[i][2], triangleArrays[i+1][2], triangleArrays[i+2][2]]);
+        }
 
-        return isValidTriangle ? validCount + 1 : validCount;
-    }, 0);
-
-    console.log(numValid);
+        console.log('Part 2 Answer:', countValidTriangles(altTriangles));
+    }
 })
