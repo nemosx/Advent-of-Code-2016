dc8e9429a2c117d04234653c3eba8a847bd88bdf
nemosx <root.michael.r@gmail.com>
2016-12-11 14:36:35 -0500

    Day 6 Part 1 Cleanup

--- src/Day6/part1.js
+++ src/Day6/part1.js
@@ -3,29 +3,20 @@ const columns = [];
 
 fs.readFileSync('input.txt', 'utf-8')
     .split('\n')
-    .forEach(line =>  {
+    .forEach(line => {
         line.split('')
             .map((currentValue, index) => {
-                //check to see if a map exists @ the index, if not, create one and insert it
-                if (!columns[index]) {
-                   columns[index] = new Map();
-                }
-                let map = columns[index];
-
-                let count = map.get(currentValue);
-
-                if (!count) {
-                    count = 0;
-                }
+                let map = columns[index] = (columns[index] || new Map());
+                let count = map.get(currentValue) || 0;
                 map.set(currentValue, count + 1);
             })
     });
 
 const answer = columns.map(countMap => {
-   return Array.from(countMap.entries())
-       .sort((a, b) => {
+    return Array.from(countMap.entries())
+        .sort((a, b) => {
             return b[1] - a[1];
-       })[0][0];
+        })[0][0];
 }).join('');
 
 console.log(answer);
